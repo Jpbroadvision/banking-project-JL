970c5b2d0794f0818c8a4865cdf281ad396ff6b3
joanalawer <lawerjoana477@gmnail.com>
2023-01-11 06:22:32 +0000

    Update templates folder

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/home/users/<string:name>/posts/<int:id>')
+# def hello(name, id):
+#     return 'Hello, ' + name + ', your id is: ' + str(id)
+
+# @app.route('/onlyget', methods=['GET'])
+# def get_req():
+#     return 'You can only get this webpage!.'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
