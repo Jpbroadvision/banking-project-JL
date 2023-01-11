a2162262672b25647760a63e2df8a344391e9e1b
joanalawer <lawerjoana477@gmnail.com>
2023-01-11 10:31:29 +0000

    Templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+boostrap = Bootstrap(app)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
     return render_template('index.html')
 
 if __name__ == '__main__':
