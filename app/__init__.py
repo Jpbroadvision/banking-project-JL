3dacd08e75cc30c50e0527267243fbf25784efa6
joanalawer <lawerjoana477@gmnail.com>
2023-01-11 23:09:07 +0000

    Banking activity pages.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -8,9 +8,21 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route('/new_account')
+def new_account():
+    return render_template('new_account.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
