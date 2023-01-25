120856dd6ea6f517e03fe185c8ff09cf1b0b1720
Jpbroadvision <paulkpantey@gmail.com>
2023-01-25 15:12:31 +0000

    Refactor config and clean ups

--- config.py
+++ config.py
@@ -1,39 +1,31 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))    # Returns the banking-project directory and directory of specified files
                                                         # making its content available to the flask app
-
+from decouple import config
 class Config:           
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir, 'test_customers.sqlite3')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
+    SECRET_KEY = config('SECRET_KEY', default= os.environ.get('SECRET_KEY'))
+    
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev_customers.sqlite3')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get('DEV_DATABASE_URL'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
 
     
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.sqlite3')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get('TEST_DATABASE_URL'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
 
     
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'customers.sqlite3')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get('DATABASE_URL'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     
-config = {
+config_dict = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
+    'production': ProductionConfig
 }
