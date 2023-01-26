e10a2d687aeac1262b53d35bd3c8a345f3f24588
Jpbroadvision <paulkpantey@gmail.com>
2023-01-26 08:07:26 +0000

    Add user model

--- main/models.py
+++ main/models.py
@@ -1,19 +1,47 @@
-from flask_login import LoginManager, UserMixin
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from __init__ import login_manager, db
+import datetime
+from sqlalchemy import DateTime
 
-class Customer(UserMixin):
+class Users(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    acc_number = db.Column(db.Integer, unique=True, index=True)
+    password = db.Column(db.String(128))
+    is_active = db.Column(db.Boolean)
+    last_login = db.Column(DateTime, default=datetime.datetime.now )
+
+
+    def __init__(self, **kwargs):
+        for property, value in kwargs.items():
+            if hasattr(value, '__iter__') and not isinstance(value, str):
+                value = value[0]
+            setattr(self, property, value)
+    def get_id(self):
+        return (self.id)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+class Customer(db.Model):
     __tablename__ = 'customers'
-    id = db.Column(db.Integer, primary_key = True)
-    firstname = db.Column(db.String(64), unique=True, index=True)
-    lastname = db.Column(db.String(64), unique=True, index=True)
-    othername = db.Column(db.String(64), unique=True, index=True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    firstname = db.Column(db.String(64), index=True)
+    othername = db.Column(db.String(64),  index=True)
+    lastname = db.Column(db.String(64), index=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(64), unique=True, index=True)
-    password = db.Column(db.String(128))
-    address = db.Column(db.String(128), unique=True, index=True)
+    address = db.Column(db.String(128), index=True)
     phone_number = db.Column(db.Integer, unique=True, index=True)
-    acc_number = db.Column(db.Integer, unique=True, index=True)
+    
 
 
     def __init__(self, **kwargs):
@@ -24,4 +52,4 @@ class Customer(UserMixin):
 
 @login_manager.user_loader
 def load_user(id):
-    return Customer.query.filter_by(id=id).first()
+    return Users.query.filter_by(id=id).first()
